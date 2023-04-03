566dc5fc7432d1d8bb29d8499384bf5efcf66c48
KronosTheTitan <tjalle@borgersonline.nl>
2023-04-03 14:21:37 +0200

    basic collectable implementation

---
+++ Assets/Scripts/CollectableManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class CollectableManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static CollectableManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    #endregion
+    
+    [SerializeField] private int treasureCollected;
+
+    public void PickupTreasure()
+    {
+        treasureCollected++;
+    }
+}
