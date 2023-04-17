30c5bdf58a3c970ecd6755254b99278600470222
StoneyMaroney <neuweilerthomas@gmail.com>
2023-04-17 12:11:24 +0200

    Added fake Calibration Loading screen

---
+++ Assets/AssetsCentre/04. UI/Screens/calibrationScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class calibrationScene : MonoBehaviour
+{
+
+   public GameObject waitScreen;
+    public GameObject calibrationScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(calibrate());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   IEnumerator calibrate()
+    {
+
+        yield return new WaitForSeconds(3);
+        calibrationScreen.gameObject.SetActive(true);
+        waitScreen.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("Final Scene");
+
+
+    }
+}
